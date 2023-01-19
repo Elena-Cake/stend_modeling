55c673240614cd4ebd83bb6bdd46ea29bc74b3fc
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-01-19 15:19:23 +0300

    popups

--- src/components/AddNSPopup/AddNSPopup.jsx
+++ src/components/AddNSPopup/AddNSPopup.jsx
@@ -5,15 +5,31 @@ const AddNSPopup = ({ isOpen, onClose }) => {
     const classPopup = `popup popup-addNS  ${isOpen ? 'popup_opened' : ''}`
     return (
         <div className={classPopup}>
-            <div className="popup__container">
+            <div className="popup__container popup__addNS">
                 <button onClick={onClose}
                     className="popup__btn-close"
                     type="button"
                     aria-label="закрыть окно">
                 </button>
-                <form className="popup__form" name="addPopup" >
+                <form className="popup__form form__addNS" name="addNS" >
                     <h3 className="popup__title">Добавить телескоп</h3>
-                    <button className="button__bottom">Добавить в БД</button>
+                    <label className="form__field">
+                        <p className="form__label">название</p>
+                        <input
+                            className="form__input"
+                            placeholder="ведите название"
+                            required
+                        />
+                    </label>
+                    <label className="form__field">
+                        <p className="form__label">что-то важное</p>
+                        <input
+                            className="form__input"
+                            placeholder="ведите что-то важное"
+                            required
+                        />
+                    </label>
+                    <button className="button__bottom form__btn">Добавить в БД</button>
                 </form>
             </div>
         </div>

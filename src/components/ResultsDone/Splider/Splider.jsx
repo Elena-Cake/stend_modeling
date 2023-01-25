cb4d62103bee9bfad5afd961421d497608310eef
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-01-25 11:55:05 +0300

    charts structure

---
+++ src/components/ResultsDone/Splider/Splider.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Charts from "./Charts/Charts";
+import './Splider.css';
+
+const Splider = ({ }) => {
+
+    return (
+        <div>
+            <Charts />
+            <Charts />
+            <Charts />
+        </div>
+    )
+}
+
+export default Splider;
+

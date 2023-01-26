da3082b8e5432e4fdff3816747eed05c23841f23
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-01-26 13:39:08 +0300

    clean all tables

--- src/components/ResultsDone/TableResultsDone/TableResultsDone.jsx
+++ src/components/ResultsDone/TableResultsDone/TableResultsDone.jsx
@@ -7,6 +7,30 @@ import configuration from '../../../constans/configurations'
 
 const TableResultsDone = ({ }) => {
 
+    const changeResStructure = (res) => {
+        const rowDataGenerated = [];
+        for (let key in res.configurations) {
+            for (let keyInst in res.configurations[key].instruments) {
+                const item = {
+                    idInstruments: keyInst,
+                    name: res.configurations[key].instruments[keyInst].name,
+                    latitude: res.configurations[key].instruments[keyInst].latitude,
+                    longitude: res.configurations[key].instruments[keyInst].longitude,
+                    mode: res.configurations[key].instruments[keyInst].mode,
+                    voko: res.configurations[key].instruments[keyInst].voko,
+                    noko: res.configurations[key].instruments[keyInst].noko,
+                    noko_twilight: res.configurations[key].instruments[keyInst].noko_twilight,
+                    gso_survey: res.configurations[key].instruments[keyInst].gso_survey
+                }
+                rowDataGenerated.push(item)
+            }
+        }
+        return rowDataGenerated
+    }
+
+    console.log(changeResStructure(configuration))
+
+
 
     return (
         <div >
